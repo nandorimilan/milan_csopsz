4b07dcc82a5bd5075cdc496264ca16175e050bb0
Nándori Milán <105380098+nandorimilan@users.noreply.github.com>
2026-01-31 12:51:24 +0100

    Create gyak_mosolygo_fuggvenyek_rov_megertes.py

---
+++ gyak_mosolygo_fuggvenyek_rov_megertes.py
@@ -0,0 +1,38 @@
+# def lényege -> definition
+
+def írdKiEzt(szöveg:str="Nem adtál meg semmit"): # Mit csinaljunk? Írjuk ki! De mit írjunk ki? Ami a zárójelben van! 2. Mindeképpen szöveget akarunk kiírni, akkor str lesz (vagyis string). De ha üresen adjuk be, akkor legyen alapértelmezetten "Nem adtál meg semmit ez fogja kiírni"
+    print(szöveg)
+
+írdKiEzt('Nem tudom mit írjak ki') # Most hívjuk meg, hogy végbe menjen amit kértünk tőle, vagyis írassa ki amit akarunk
+írdKiEzt() # Most nézzük meg, mivan ha semmit se írunk bele, akkor alapértelmezett választ kapjuk meg vagyis ="Nem adtál meg semmit"
+
+#Logika
+# def (ez egy fuggveny igy adjuk meg)
+# def kell neki egy név amire hivatkozunk, név(): FONTOS LEGYEN MOGOTTE MINDIG ()!!! Még meghívásnál is, csak ott : ot ne írd oda 
+# def név(paraméterek jönnek ide)
+# def név(paraméter (milyen legyen? ) str, int, bool, list)
+# def név(paraméter:str)
+# def név(paraméter:str Legyen alapértelmezettje? paraméter:str="Ha nem kapunk paramétert, ezt használjuk")
+# def név(paraméter:str="Nem kaptunk paramétert"): # Mit adjuk vissza, (return -al) str, int, bool, list 
+# def név(paraméter:str="Nem kaptunk paramétert") -> int: 
+# def név(paraméter:str="Nem kaptunk paramétert") -> int:
+    # Ide jön mit akarunk lefutattni
+
+
+
+
+# 1: Generálj egy függvény tetszöleges névvel, ami megfog 3 számot majd összeadja egy nagy számmá, és ezt tárold el egy változoba! Fontos Számként adja vissza
+
+
+
+
+
+
+
+
+# 2: Most használjuk fel az előzöleg létrehozott változot, es csináljunk egy szorazott. Hozz létre egy tetszőleges névvel egy függvényt, aminek egyik paramétere ez előzöleg létrehozott változó,
+# A másik legyen egy szorzó, amely alapértéke (ha nem adunk meg semmit) legyen 2 
+# Majd amit visszakaptunk számot írassuk ki! print(számologep(mergedNumber, 5))
+
+
+
